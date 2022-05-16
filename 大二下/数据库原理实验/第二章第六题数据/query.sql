b7d5afdbce09035846694c0a5c88affc4a02f32e
dmucby <cby@dlmu.edu.cn>
2022-05-16 16:55:58 +0800

    update

---
+++ 大二下/数据库原理实验/第二章第六题数据/query.sql
@@ -0,0 +1,92 @@
+--(1)找出所有供应商的姓名和所在城市。
+SELECT S.SNAME ,S.CITY 
+FROM S;
+
+--(2)找出所有零件的名称、颜色、重量。
+SELECT P.PNAME ,P.COLOR ,P.WEIGHT 
+FROM P;
+
+--(3)找出使用供应商S1所供应零件的工程号码。
+SELECT SPJ.JNO
+FROM SPJ 
+WHERE SPJ.SNO = 'S1';
+
+--(4)找出工程项目J2使用的各种零件的名称及其数量。
+SELECT DISTINCT  P.PNAME ,SPJ.QTY 
+FROM SPJ,P
+WHERE SPJ.JNO = 'J2' 
+AND P.PNO = SPJ.PNO;
+
+SELECT DISTINCT P.PNAME ,SPJ.QTY 
+FROM SPJ
+JOIN P
+ON P.PNO = SPJ.PNO 
+WHERE SPJ.JNO = 'J2';
+
+--(5)找出上海厂商供应的所有零件号码。
+SELECT DISTINCT  SPJ.PNO 
+FROM S 
+JOIN SPJ
+ON SPJ.SNO = S.SNO 
+WHERE S.CITY ='上海';
+
+--(6)找出使用上海产的零件的工程名称。
+SELECT J.JNAME
+FROM J 
+WHERE J.JNO IN (
+		SELECT DISTINCT  SPJ.JNO 
+		FROM S 
+		JOIN SPJ
+		ON SPJ.SNO = S.SNO 
+		WHERE S.CITY ='上海'
+);
+
+
+--(7)找出没有使用天津产的零件的工程号码。
+SELECT J.JNAME,j.JNO 
+FROM J 
+WHERE J.JNO not IN (
+		SELECT DISTINCT  SPJ.JNO 
+		FROM S 
+		JOIN SPJ
+		ON SPJ.SNO = S.SNO 
+		WHERE S.CITY ='天津'
+);
+
+
+--(8)把全部红色零件的颜色改成蓝色。
+UPDATE P
+SET P.COLOR = '蓝'
+WHERE P.COLOR  = '红';
+
+--(9)由S5供给J4的零件P6改为由S3供应。
+UPDATE SPJ 
+SET SPJ.SNO = 'S3'
+WHERE SPJ.SNO = 'S5'
+AND SPJ.JNO = 'J4'
+AND PNO = 'P4';
+
+--(10)从供应商关系中删除供应商号是S2的记录，并从供应情况关系中删除相应的记录。
+DELETE 
+FROM S  -- 注意加上FROM
+WHERE S.SNO = 'S2';
+
+DELETE 
+FROM SPJ 
+WHERE SPJ.SNO ='S2';
+
+--(11)请将(S2，J6，P4，200)插入供应情况关系。
+INSERT 
+INTO SPJ
+VALUES 
+('S2','J6','P4',200);
+
+SELECT SPJ.SNO
+FROM SPJ 
+WHERE SPJ.SNO ='S2'
+AND SPJ.JNO = 'J6';
+
+
+
+
+
